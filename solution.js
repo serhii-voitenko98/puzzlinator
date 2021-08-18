b564dfd1e499b50bb56a3455e27d96a77ebc2c97
Serhii Voitenko <serhii.voitenko98@gmail.com>
2021-08-18 20:07:00 +0300

    do refactoring

--- solution.js
+++ solution.js
@@ -1,86 +1,4 @@
-const puzzles = [
-	{
-		id: 1,
-		edges: {
-			top: null,
-			right: null,
-			bottom: { edgeTypeId: 7, type: "outside" },
-			left: { edgeTypeId: 5, type: "inside" },
-		},
-	},
-	{
-		id: 9,
-		edges: {
-			top: { edgeTypeId: 8, type: "inside" },
-			right: { edgeTypeId: 15, type: "inside" },
-			bottom: null,
-			left: { edgeTypeId: 5, type: "outside" },
-		},
-	},
-	{
-		id: 5,
-		edges: {
-			top: null,
-			right: { edgeTypeId: 2, type: "inside" },
-			bottom: { edgeTypeId: 1, type: "inside" },
-			left: null,
-		},
-	},
-	{
-		id: 4,
-		edges: {
-			top: { edgeTypeId: 34, type: "inside" },
-			right: { edgeTypeId: 11, type: "outside" },
-			bottom: { edgeTypeId: 7, type: "inside" },
-			left: null,
-		},
-	},
-	{
-		id: 3,
-		edges: {
-			top: { edgeTypeId: 2, type: "outside" },
-			right: null,
-			bottom: { edgeTypeId: 4, type: "outside" },
-			left: { edgeTypeId: 6, type: "inside" },
-		},
-	},
-	{
-		id: 2,
-		edges: {
-			top: { edgeTypeId: 3, type: "outside" },
-			right: { edgeTypeId: 34, type: "outside" },
-			bottom: null,
-			left: null,
-		},
-	},
-	{
-		id: 8,
-		edges: {
-			top: null,
-			right: { edgeTypeId: 15, type: "outside" },
-			bottom: { edgeTypeId: 4, type: "inside" },
-			left: null,
-		},
-	},
-	{
-		id: 7,
-		edges: {
-			top: { edgeTypeId: 3, type: "inside" },
-			right: null,
-			bottom: { edgeTypeId: 1, type: "outside" },
-			left: { edgeTypeId: 10, type: "inside" },
-		},
-	},
-	{
-		id: 6,
-		edges: {
-			top: { edgeTypeId: 11, type: "inside" },
-			right: { edgeTypeId: 10, type: "outside" },
-			bottom: { edgeTypeId: 6, type: "outside" },
-			left: { edgeTypeId: 8, type: "outside" },
-		},
-	},
-];
+const { puzzles } = require('./puzzles');
 
 const firstPuzzle = puzzles[0];
 const rightId = findFirstRightEdgeTypeId(firstPuzzle.edges);
@@ -207,5 +125,4 @@ function isPuzzleIncludesEdgeTypeId(puzzle, id) {
 	return !!edgesKeys.filter(edge => edges[edge] ? edges[edge].edgeTypeId === id : false).length;
 }
 
-console.log(solvePuzzle(puzzles))
-
+console.log(solvePuzzle(puzzles));
